49c1a008d2e20ca9b39220f0453d9669ee93e874
AlexsanderBenatti <abenatti.silva@gmail.com>
2022-06-21 09:58:26 -0300

    Prontinho

--- app/src/main/java/com/example/newnavigation/firstFragment.kt
+++ app/src/main/java/com/example/newnavigation/firstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.Navigation
 
 class firstFragment : Fragment() {
@@ -15,8 +16,11 @@ class firstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_first, container, false)
+        val button1 = view.findViewById<Button>(R.id.fragmentLeft_button)
+        val button2 = view.findViewById<Button>(R.id.fragmentRight_button)
 
-        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_thirdFragment)}
+        button1.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment)}
+        button2.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_thirdFragment)}
 
         return view
     }
